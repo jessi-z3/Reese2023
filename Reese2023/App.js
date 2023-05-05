496708e368a54e827d200e1a90368878cbe02ac5
jessi-z3 <jessi_leeann@yahoo.com>
2023-05-05 15:33:25 -0500

    UGH

--- Reese2023/App.js
+++ Reese2023/App.js
@@ -1,109 +1,81 @@
+import { DancingScript_500Medium } from "@expo-google-fonts/dancing-script";
+import { Gabriela_400Regular } from "@expo-google-fonts/gabriela";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import React from "react";
+import { View, Text, Image, Pressable } from "react";
+import * as React from "react";
 
-import * as Font from 'expo-font';
+import { useFonts } from "expo-font";
 
-import {View, Text, Image, Pressable} from React;
+import {styles}  from "./Styles";
 
-import {styles} from "Styles.js"
 const Stack = createNativeStackNavigator();
-
-
-
-export default class App extends React.Component {
-  render() {
+export default function App(){
+   useFonts({
+     'dancing': DancingScript_500Medium,
+     'gabriela': Gabriela_400Regular,
+   });
     return (
+      <>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Main">
-          <Stack.Screen name="Main" component={<Main {...mainData} />} />
+          <Stack.Screen name="Main" component={Main}/>
         </Stack.Navigator>
       </NavigationContainer>
+      </>
     );
-  }
 }
 
-function Main(props) {
-  const {
-    intersect,
-    spanText1,
-    spanText2,
-    spanText3,
-    spanText4,
-    spanText5,
-    spanText6,
-    mission,
-    beBRAVE,
-    program,
-    pledge,
-    braveLogo,
-  } = props;
-  useEffect(() => {
-    async function loadFont() {
-      await Font.loadAsync({
-        'dancing script': require('./assets/fonts/Dancing_Script/DancingScript-VariableFont_wght.ttf'),
-        'gabriela': require('./assets/fonts/Gabriela/Gabriela-Regular.ttf'),
-      });
-
-      Text.defaultProps.style.fontFamily = 'dancing script';
-    }
-
-    loadFont();
-  }, []);
-
-
-    loadFont();
+function Main(){
   return (
-    <View style = {styles.overlapGroup4}>
-      <View style = {styles.overlapGroup5}>
-        <View style = {styles.reese}>
-          <Image style = {styles.intersect} src={intersect} alt="Intersect" />
+    <View style={styles.overlapGroup4}>
+      <View style={styles.overlapGroup5}>
+        <View style={styles.reese}>
+          <Image style={styles.intersect1} source="./img/intersect2x.png" />
         </View>
-        <Text style = {styles.title}>
-          <Text style = {styles.span0}>{spanText1}</Text>
-          <Text style = {styles.span1}>{spanText2}</Text>
-          <Text style = {styles.span2}>{spanText3}</Text>
-          <Text style = {styles.span3}>{spanText4}</Text>
-          <Text style = {styles.span1}>{spanText5}</Text>
-          <Text style = {styles.span2}>{spanText6}</Text>
+        <Text style={styles.title}>
+          <Text style={styles.span0}>"R"</Text>
+          <Text style={styles.span1}>"e"</Text>
+          <Text style={styles.span2}>"es"</Text>
+          <Text style={styles.span3}>"e-o"</Text>
+          <Text style={styles.span1}>"urc"</Text>
+          <Text style={styles.span2}>"es"</Text>
         </Text>
       </View>
-      <View style = {styles.buttons}>
-        <View style = {styles.overlapGroupContainer}>
-          <View style = {styles.overlapGroup2}>
-            <Pressable style = {styles.mission}>{mission}</Pressable>
+      <View style={styles.buttons}>
+        <View style={styles.overlapGroupContainer}>
+          <View style={styles.overlapGroup2}>
+            <Pressable style={styles.mission}>"Mission"</Pressable>
           </View>
-          <View style = {styles.overlapGroup3}>
-            <Pressable style = {styles.beBRAVE}>{beBRAVE}</Pressable>
+          <View style={styles.overlapGroup3}>
+            <Pressable style={styles.beBRAVE}>"Be B.R.A.V.E."</Pressable>
           </View>
         </View>
-        <View style = {styles.overlapGroupContainer}>
-          <View style = {styles.overlapGroup1}>
-            <Pressable style = {styles.program}>{program}</Pressable>
+        <View style={styles.overlapGroupContainer}>
+          <View style={styles.overlapGroup1}>
+            <Pressable style={styles.program}>Program</Pressable>
           </View>
-          <View style = {styles.overlapGroup}>
-            <Pressable style = {styles.pledge}>{pledge}</Pressable>
+          <View style={styles.overlapGroup}>
+            <Pressable style={styles.pledge}>Pledge</Pressable>
           </View>
         </View>
       </View>
-      <Image style = {styles.braveLogo} src={braveLogo} alt="BRAVE logo"/>
+      <Image
+        style={styles.braveLogo}
+        source="./img/brave-logo2x.png"
+        alt="BRAVE logo"
+      />
     </View>
   );
 }
 
 
-const mainData = {
-  intersect: "Reese2023/assets/intersect@2x.png",
-  spanText1: "R",
-  spanText2: "e",
-  spanText3: "es",
-  spanText4: "e-o",
-  spanText5: "Urc",
-  spanText6: "es",
-  mission: "Mission",
-  beBRAVE: "Be B.R.A.V.E.",
-  program: "Program",
-  pledge: "Pledge",
-  braveLogo: "Reese2023/assets/brave-logo@2x.png",
-};
+
+
+
+
+
+
+
---
+++ Reese2023/Main.js
@@ -0,0 +1,56 @@
+// import {DancingScript_500Medium} from '@expo-google-fonts/dancing-script';
+// import {Gabriela_400Regular} from '@expo-google-fonts/gabriela';
+
+// import { View, Text, Image, Pressable } from "react";
+// import * as React from "react";
+
+// import { useFonts } from "expo-font";
+
+// import { styles } from "./Styles";
+
+// export default function Main() {
+//    useFonts({
+//     'dancing script': DancingScript_500Medium,
+//     'gabriela': Gabriela_400Regular,
+//   }); 
+//   return (
+//     <View style={styles.overlapGroup4}>
+//       <View style={styles.overlapGroup5}>
+//         <View style={styles.reese}>
+//           <Image style={styles.intersect1} source="./img/intersect2x.png" />
+//         </View>
+//         <Text style={styles.title}>
+//           <Text style={styles.span0}>R</Text>
+//           <Text style={styles.span1}>e</Text>
+//           <Text style={styles.span2}>es</Text>
+//           <Text style={styles.span3}>e-o</Text>
+//           <Text style={styles.span1}>urc</Text>
+//           <Text style={styles.span2}>es</Text>
+//         </Text>
+//       </View>
+//       <View style={styles.buttons}>
+//         <View style={styles.overlapGroupContainer}>
+//           <View style={styles.overlapGroup2}>
+//             <Pressable style={styles.mission}>Mission</Pressable>
+//           </View>
+//           <View style={styles.overlapGroup3}>
+//             <Pressable style={styles.beBRAVE}>Be B.R.A.V.E.</Pressable>
+//           </View>
+//         </View>
+//         <View style={styles.overlapGroupContainer}>
+//           <View style={styles.overlapGroup1}>
+//             <Pressable style={styles.program}>Program</Pressable>
+//           </View>
+//           <View style={styles.overlapGroup}>
+//             <Pressable style={styles.pledge}>Pledge</Pressable>
+//           </View>
+//         </View>
+//       </View>
+//       <Image
+//         style={styles.braveLogo}
+//         source="./img/brave-logo2x.png"
+//         alt="BRAVE logo"
+//       />
+//     </View>
+//   );
+// }
